22785d233b9594273d0af11f90b86b3a165db568
eunsukko <47235893+eunsukko@users.noreply.github.com>
2022-12-13 21:47:54 +0900

    feat: Isbn 을 구현해요 - Isbn의_글자수는_10자리_혹은_13자리만_가능해요

--- src/main/java/com/github/eunsukko/domain/Isbn.java
+++ src/main/java/com/github/eunsukko/domain/Isbn.java
@@ -1,12 +1,37 @@
 package com.github.eunsukko.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public class Isbn {
     private final String isbn13;
 
     public static Isbn from(String isbnString) {
+        if (isbnString.length() != 10
+            && isbnString.length() != 13) {
+            throw new IllegalArgumentException("Isbn 은 10 or 13 자리의 숫자입니다");
+        }
         return new Isbn(isbnString);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Isbn isbn = (Isbn) o;
+        return Objects.equals(isbn13, isbn.isbn13);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn13);
+    }
+
+    @Override
+    public String toString() {
+        return isbn13;
     }
 }
--- src/test/java/com/github/eunsukko/domain/IsbnTest.java
+++ src/test/java/com/github/eunsukko/domain/IsbnTest.java
@@ -2,17 +2,51 @@ package com.github.eunsukko.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 
 class IsbnTest {
 
     @Test
-    public void Isbn은_10자리의_숫자_혹은_13자리의_숫자여야해요() {
+    public void Isbn은_13자리_숫자로_만들수있어요() {
+        String 숫자_13자리 = "9791187506072";
+
+        var isbn = Isbn.from(숫자_13자리);
+
+        assertThat(isbn.toString())
+                .isEqualTo(숫자_13자리);
+    }
+
+    @Test
+    public void Isbn을_10자리_숫자로_만들경우_13자리로_채워주어요() {
+//        assertThat()
 
     }
 
     @Test
-    public void Isbn이_10자리인_경우_13자리로_채워주어요() {
+    public void Isbn의_글자수는_10자리_혹은_13자리만_가능해요() {
+        String isbn_이_될수없는_글자수 = "1";
+
+        assertThatThrownBy(() -> Isbn.from(isbn_이_될수없는_글자수))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Isbn은_영문을_포함할수_없어요() {
+        String 영문을_포함한_문자열 = "aaa1234567890";
+
+        assertThatThrownBy(() -> Isbn.from(영문을_포함한_문자열))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Isbn은_특수문자를_포함할수_없어요() {
+
+    }
+
+    @Test
+    public void Isbn은_한글을_포함할수_없어요() {
 
     }
 }
