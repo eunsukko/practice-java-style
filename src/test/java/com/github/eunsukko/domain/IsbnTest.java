3abad091d59298c4f2203e52546e6a0df853aab0
eunsukko <47235893+eunsukko@users.noreply.github.com>
2022-12-16 01:09:53 +0900

    feat: Isbn 을 구현해요 - Isbn10 이 숫자로 이뤄졌는지를 확인해요

--- src/test/java/com/github/eunsukko/domain/IsbnTest.java
+++ src/test/java/com/github/eunsukko/domain/IsbnTest.java
@@ -18,43 +18,26 @@ class IsbnTest {
                 .isEqualTo(숫자_13자리);
     }
 
+    // 출처: 나무위키
+    // 1. 13 자리 ISBN을 (A,B,C,D,E,F,G,H,I,J,K,L,M) 라 하고, 이 벡터를 b 라 한다.
+    // 2. a = (1,3,1,3,1,3,1,3,1,3,1,3,1) 라 한다.
+    // 3. ( a · b ) % 10 의 값이 0이 되어야 한다.
+    @Test
+    public void Isbn13이_유효한지를_체크번호를_통해_확인해요() {
+        String 숫자_13자리 = "9791187506072";
+
+        var isbn = Isbn.from(숫자_13자리);
+
+        assertThat(isbn.toString())
+                .isEqualTo(숫자_13자리);
+    }
+
     @Test
     public void Isbn을_10자리_숫자로_만들수있어요() {
-//        assertThat()
-        // 정상적으로 생성되는지
-        // 13자리로 기대하는 값이 나오는지
-    }
+        String 숫자_10자리 = "8991268072";
 
-    @Test
-    public void Isbn의_글자수는_10자리_혹은_13자리만_가능해요() {
-        String isbn_이_될수없는_글자수 = "1";
-
-        assertThatThrownBy(() -> Isbn.from(isbn_이_될수없는_글자수))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    public void Isbn은_영문을_포함할수_없어요() {
-        String 영문을_포함한_문자열 = "aaa1234567890";
-
-        assertThatThrownBy(() -> Isbn.from(영문을_포함한_문자열))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    public void Isbn은_특수문자를_포함할수_없어요() {
-        String 특수문자를_포함한_문자열 = "!001234567890";
-
-        assertThatThrownBy(() -> Isbn.from(특수문자를_포함한_문자열))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    public void Isbn은_한글을_포함할수_없어요() {
-        String 한글을_포함한_문자열 = "가001234567890";
-
-        assertThatThrownBy(() -> Isbn.from(한글을_포함한_문자열))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(Isbn.from(숫자_10자리).toString())
+                .isEqualTo("9788991268074");
     }
 
     // 출처: 나무위키
@@ -66,13 +49,62 @@ class IsbnTest {
 
     }
 
-    // 출처: 나무위키
-    // 1. 13 자리 ISBN을 (A,B,C,D,E,F,G,H,I,J,K,L,M) 라 하고, 이 벡터를 b 라 한다.
-    // 2. a = (1,3,1,3,1,3,1,3,1,3,1,3,1) 라 한다.
-    // 3. ( a · b ) % 10 의 값이 0이 되어야 한다.
-    @Test
-    public void Isbn13이_유효한지를_체크번호를_통해_확인해요() {
 
+    @Test
+    public void Isbn은_9자리_숫자와_마지막에_X로_만들수있어요() {
+        String 숫자_9자리와_마지막에_X = "899126803X";
+
+        assertThat(Isbn.from(숫자_9자리와_마지막에_X).toString())
+                .isEqualTo("9788991268036");
     }
+
+    @Test
+    public void Isbn의_글자수는_10자리_혹은_13자리만_가능해요() {
+        String isbn_이_될수없는_글자수 = "1";
+
+        assertThatThrownBy(() -> Isbn.from(isbn_이_될수없는_글자수))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Isbn13은_영문을_포함할수_없어요() {
+        String 영문을_포함한_문자열 = "aaa1234567890";
+
+        assertThatThrownBy(() -> Isbn.from(영문을_포함한_문자열))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Isbn13은_특수문자를_포함할수_없어요() {
+        String 특수문자를_포함한_문자열 = "!001234567890";
+
+        assertThatThrownBy(() -> Isbn.from(특수문자를_포함한_문자열))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Isbn10은_마지막자리_이외에는_영문을_포함할수_없어요() {
+        String 영문을_포함한_문자열 = "a234567890";
+
+        assertThatThrownBy(() -> Isbn.from(영문을_포함한_문자열))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Isbn10은_특수문자를_포함할수_없어요() {
+        String 특수문자를_포함한_문자열 = "!234567890";
+
+        assertThatThrownBy(() -> Isbn.from(특수문자를_포함한_문자열))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Isbn10은_한글을_포함할수_없어요() {
+        String 한글을_포함한_문자열 = "가234567890";
+
+        assertThatThrownBy(() -> Isbn.from(한글을_포함한_문자열))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
